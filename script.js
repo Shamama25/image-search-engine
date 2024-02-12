d1ff0b70c8d65c314d2facff108c1e83b641990c
Shamama25 <131394869+Shamama25@users.noreply.github.com>
2024-02-12 17:04:50 +0000

    updated form for fetching API

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-const accessKey = "_eSN_ct7DsOdsyvRQjmMY-8l5MBoelWvuTxjn5bHXiM";
-
+const accessKey = "_eSN_ct7DsOdsyvRQjmMY-8l5MBoelWvuTxjn5bHXiM"
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
-const searchResult = document.getElementById("search-result");
+const searchResult = document.querySelector(".search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
-
-let keyword = "";
 let page = 1;
 
-async function searchImages(){
+async function searchImages() {
     keyword = searchBox.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
 
-    const results = data.results;
+        if (!response.ok) {
+            throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
+        }
 
-    if(page === 1){
-        searchResult.innerHTML = "";
+        const data = await response.json();
+
+        console.log("API response", data);
+
+        const results = data.results;
+
+        if (page === 1) {
+            searchResult.innerHTML = "";
+        }
+
+        results.forEach((result) => {
+            const image = document.createElement("img");
+            image.src = result.urls.small;
+
+            const imageLink = document.createElement("a");
+            imageLink.href = result.links.html;
+            imageLink.target = "_blank";
+
+            imageLink.appendChild(image);
+            searchResult.appendChild(imageLink);
+        });
+
+        showMoreBtn.style.display = "block";
+    } catch (error) {
+        console.error("Error fetching data:", error);
     }
-    results.map((result) =>{
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
-
-        imageLink.appendChild(image);
-        searchResult.appendChild(imageLink);
-    })
-    showMoreBtn.style.display = "block";
 }
 
-searchForm.addEventListener("submit", (e) =>{
+searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("Form submitted");
     page = 1;
     searchImages();
-})
+});
 
 showMoreBtn.addEventListener("click", () => {
     page++;
